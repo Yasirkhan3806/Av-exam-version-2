88c6ab99d0e6602f798787dd6146066ea0ac59e0
Yasirkhan3806 <yk5645123@gmail.com>
2026-05-19 16:10:07 +0500

    a few changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,9 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NEXT_PUBLIC_MODE === 'production',
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
